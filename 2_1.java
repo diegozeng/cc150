9800021eed13580873b06d76f7244dd54a3e1c5b
diegozeng <diegozengtao@gmail.com>
2014-09-03 15:58:36 +0800

    update 2_1.java

--- 2_1.java
+++ 2_1.java
@@ -2,14 +2,15 @@
  * 2.1 Write code to remove duplicates from an unsorted linked list. FOLLOW UP
  * How would you solve this problem if a temporary buffer is not allowed?
  *
- * 思路:遍历链表，相邻Node储存的元素进行两两比较，若相同则把LinkedListNode.next指向LinkedListNode.next.next.
+ * 思路: 除重复，HashSet可破.
+ *      Follow up:遍历链表，相邻Node储存的元素进行两两比较，若相同则把LinkedListNode.next指向LinkedListNode.next.next.
  *
- * 时间复杂度:O(n^2);
- * 空间复杂度:O(1);
+ * 时间复杂度:O(n)/O(n^2);
+ * 空间复杂度:O(n)/O(1);
  *
  * Test case："1 -> 2 -> 2 -> 3 -> 4" deleteDups()后 变成 "1 -> 2 -> 3 -> 4"。
- * 注: 相邻的元素重复才算的上是duplicate，比如 “1 -> 2 -> 2” 算, 而 “1 -> 2 -> 1” 不算。
  */
+import java.util.*;
 
 class LinkedListNode {
 	int data;
@@ -18,6 +19,8 @@ class LinkedListNode {
 
 
 class Test21 {
+    
+    //Follow up
 	public static void deleteDups(LinkedListNode head) {
 		if (head == null) {
             return;
@@ -43,6 +46,32 @@ class Test21 {
             }
         }
     }
+    
+    
+    /*
+    // With temporary buffer (HashSet)
+    public static void deleteDups(LinkedListNode head) {
+        if (head == null)
+            return;
+        HashSet<Integer> hashset = new HashSet<Integer>();
+        LinkedListNode current = head;
+        hashset.add(head.data);
+        while(current != null) {
+            if(hashset.contains(current.data)) {
+                current = current.next;
+            }
+            else {
+                hashset.add(current.data);
+                current = current.next;
+            }
+        }
+        Iterator<Integer> iterator = hashset.iterator();
+        while(iterator.hasNext()){
+            System.out.print(iterator.next()+" ");
+        }
+    }
+    */
+
 	public static void main(String[] args) {
 		LinkedListNode l1 = new LinkedListNode();
         LinkedListNode l2 = new LinkedListNode();
@@ -52,8 +81,8 @@ class Test21 {
         l1.data = 1;
 		l2.data = 2;
 		l3.data = 2;
-        l4.data = 3;
-        l5.data = 4;
+        l4.data = 4;
+        l5.data = 1;
 		l1.next = l2;
         l2.next = l3;
         l3.next = l4;
